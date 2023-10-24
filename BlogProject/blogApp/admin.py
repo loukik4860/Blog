d50b75694d92fee8ed24aa4ae5c90c14f38e50a7
loukik4860 <loukikhajare@gmail.com>
2023-10-25 00:28:28 +0530

    school

--- BlogProject/blogApp/admin.py
+++ BlogProject/blogApp/admin.py
@@ -1,16 +1,27 @@
 from django.contrib import admin
-from .models import BlogModel, Tag
+from .models import BlogModel, Tag, BlogImage
 
 
 @admin.register(BlogModel)
 class BlogModelAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'pub_date', 'is_published')
-    list_filter = ('is_published', 'pub_date')
+    list_display = ('title', 'author', 'publish_date', 'is_published')
+    list_filter = ('is_published', 'publish_date')
     search_fields = ('title', 'content')
-    date_hierarchy = 'pub_date'
+    date_hierarchy = 'publish_date'
     filter_horizontal = ('tags',)
 
 
 @admin.register(Tag)
 class TagAdmin(admin.ModelAdmin):
     list_display = ('name',)
+
+
+class BlogImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'blog', 'caption')
+    list_filter = ('blog',)
+    search_fields = ('caption',)
+    list_per_page = 20  # Customize the number of items displayed per page
+    list_display_links = ('id', 'caption')  # Add links to these fields in the list view
+
+
+admin.site.register(BlogImage, BlogImageAdmin)
--- BlogProject/blogApp/models.py
+++ BlogProject/blogApp/models.py
@@ -1,24 +1,35 @@
 from django.db import models
 from AuthApp.models import AuthorUser
 
+
 # Create your models here.
 
 class BlogModel(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    pub_date = models.DateTimeField(auto_now_add=True)
-    author = models.ForeignKey(AuthorUser, on_delete=models.CASCADE)
-    featured_image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
-    tags = models.ManyToManyField('Tag', related_name='blogs', blank=True)
-    is_published = models.BooleanField(default=False)
+    title = models.CharField(max_length=200, null=True, blank=True)
+    content = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    author = models.ForeignKey(AuthorUser, on_delete=models.CASCADE, null=True, blank=True)
+    tags = models.ManyToManyField('Tag', related_name='blogs', blank=True, null=True)
+    is_published = models.BooleanField(default=False, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return f"{self.title}--{self.id}"
+
+
+class BlogImage(models.Model):
+    blog = models.ForeignKey(BlogModel, on_delete=models.CASCADE, related_name='images')
+    image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    caption = models.CharField(max_length=255, blank=True, null=True)
+
+    def __str__(self):
+        return f"Image for {self.blog.title}"
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
-
 
