182d27b5c68adf62feb9466f1342107ffdd74bc8
loukik4860 <loukikhajare@gmail.com>
2023-10-25 00:04:43 +0530

    thirdpush

--- BlogProject/blogApp/admin.py
+++ BlogProject/blogApp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import BlogModel, Tag
 
-# Register your models here.
+
+@admin.register(BlogModel)
+class BlogModelAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'pub_date', 'is_published')
+    list_filter = ('is_published', 'pub_date')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'pub_date'
+    filter_horizontal = ('tags',)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name',)
--- BlogProject/blogApp/models.py
+++ BlogProject/blogApp/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from AuthApp.models import AuthorUser
 
 # Create your models here.
+
+class BlogModel(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(AuthorUser, on_delete=models.CASCADE)
+    featured_image = models.ImageField(upload_to='blog_images/', null=True, blank=True)
+    tags = models.ManyToManyField('Tag', related_name='blogs', blank=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.title}--{self.id}"
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
